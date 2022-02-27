3f837f6e306dd38e9661da251d1c2f484bb2f835
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-28 09:57:04 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// console.log("hello".length)
+
+// const name = "Pieter-Jan"
+// console.log(name.length)
+
+// const char = "Hello".charAt(1)
+// console.log(char)
+
+// console.log("Hello" + " World")
+
+// const myName = "Pete"
+// console.log(`Hello, my Name is ${myName}`)
+
+// console.log(Math.floor(1.2))
+// console.log(Math.ceil(1.2))
+// console.log(Math.PI)
+// console.log(Math.random())
+
+console.log(5==5)
+console.log(5==4)
+console.log(4>5)
+console.log (!true)
+console.log(!false)
