195c78beec83e3c644694b13a61a724972e88590
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-28 10:43:44 +1100

    objects

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// console.log("hello".length)
+const person = {
+    name: "Matt",
+    age: 40,
+    address: {
+        city: "Sydney",
+        postcode: 2000
+    },
+    score: [15,30,40]
+}
 
-// const name = "Pieter-Jan"
-// console.log(name.length)
-
-// const char = "Hello".charAt(1)
-// console.log(char)
-
-// console.log("Hello" + " World")
-
-// const myName = "Pete"
-// console.log(`Hello, my Name is ${myName}`)
-
-// console.log(Math.floor(1.2))
-// console.log(Math.ceil(1.2))
-// console.log(Math.PI)
-// console.log(Math.random())
-
-console.log(5==5)
-console.log(5==4)
-console.log(4>5)
-console.log (!true)
-console.log(!false)
+console.log(person.name)
+console.log(person.address.city)
+console.log(person.score[0])
+console.log(person.score[1])
+console.log(person.score[2])
