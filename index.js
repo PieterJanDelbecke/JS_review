8fcca1d5e1bfa838bd117448fa81ddf2db1a0532
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-28 11:19:25 +1100

    array method find

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const arr = ["Matt",48, 4510]
+// const arr = ["Matt",48, 4510]
 
-console.log(arr[1])
-console.log(arr.length)
-console.log(arr.includes("Matt"))
-console.log(arr.includes(10))
+// console.log(arr[1])
+// console.log(arr.length)
+// console.log(arr.includes("Matt"))
+// console.log(arr.includes(10))
 
-arr.push("end")
-console.log(arr)
-arr.unshift("start")
-console.log(arr)
-console.log(arr.pop())
-console.log(arr)
-console.log(arr.shift())
-console.log(arr)
+// arr.push("end")
+// console.log(arr)
+// arr.unshift("start")
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+// console.log(arr.shift())
+// console.log(arr)
 
 
 const arr2 = [5 ,10 , 15, 30, 40]
-const values = arr.find(element => element > 15)
+const values = arr2.find(element => element > 15)
 console.log(values)
