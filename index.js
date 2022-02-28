df2cd6d0b122aeeebb6f276cc28448dbbb0c9768
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-01 08:53:18 +1100

    destructuring

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-function sumArray(array) {
-    if (!array) {
-        array = []
-    }
-    let sortArray = array.sort(function(a, b) {
-      return a - b;
-    });
-    sortArray.pop()
-    sortArray.shift()
-    let sum = 0
-    for (i=0; i < sortArray.length; i++ ){
-        sum += sortArray[i]
-    }
-    return sum
-}
+let a, b, rest;
 
-
-console.log(sumArray(null))
-console.log(sumArray([]))
-console.log(sumArray([3]))
-console.log(sumArray([3,5]))
-console.log(sumArray([6,2,1,8,10]))
-console.log(sumArray([ 0, 1, 6, 10, 10 ]))
-console.log(sumArray([ -6, -20, -1, -10, -12 ]))
+[a ,b , ...rest] = [10,20,30,40,50]
+console.log(rest)
