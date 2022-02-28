48774ee4d048088bb7a5ddafc69737c8a5b7be5c
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-01 10:23:38 +1100

    review of map

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 
 // For each
 
-let numbers = [2, 3, 4]
+// let numbers = [2, 3, 4]
 
-numbers.forEach (number => {
-    let myNumber = Math.pow(number, 2)
-    console.log(`the exponent of ${number} is ${myNumber}`)
+// numbers.forEach (number => {
+//     let myNumber = Math.pow(number, 2)
+//     console.log(`the exponent of ${number} is ${myNumber}`)
+// })
+
+let numbers = [2,3,4,5]
+
+let newNumbers = numbers.map (number => {
+    return Math.pow(number, 2)
 })
 
+console.log(newNumbers)
