c9854d9f530171088401f69bae720ab69668e864
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-28 11:41:27 +1100

    more array methods

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// const arr = ["Matt",48, 4510]
+// const arr2 = [5 ,10 , 15, 30, 40]
+// const values = arr2.find(element => element > 15)
+// console.log(values)
 
-// console.log(arr[1])
-// console.log(arr.length)
-// console.log(arr.includes("Matt"))
-// console.log(arr.includes(10))
-
-// arr.push("end")
-// console.log(arr)
-// arr.unshift("start")
-// console.log(arr)
-// console.log(arr.pop())
-// console.log(arr)
-// console.log(arr.shift())
-// console.log(arr)
-
-
-const arr2 = [5 ,10 , 15, 30, 40]
-const values = arr2.find(element => element > 15)
-console.log(values)
+let myColours =["red", "orange", "yellow", "green"]
+let word = "Himalayas are in Nepal"
+let str = " Hello World "
+// console.log(myColours[1].toUpperCase())
+// console.log(word.charAt(2))
+// console.log(word.indexOf("y"))
+// console.log(str.substring(1,3))
+const newStr = str.trim()
+console.log(newStr)
+const myStr = newStr.replace("o","x")
+console.log(myStr)
