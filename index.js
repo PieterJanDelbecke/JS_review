258f81fcea3645c19efd20b2a9ecfd09d5a9f384
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-01 09:24:59 +1100

    review of the while loop

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // FOR
 
-for (let i= 0; i <= 10; i++){
-    console.log(`I'm nuber: ${i}`)
+// for (let i= 0; i <= 10; i++){
+//     console.log(`I'm nuber: ${i}`)
+// }
+
+// WHILE
+
+let olympicMedals = 0;
+
+while (olympicMedals < 10){
+    console.log(`medal: ${olympicMedals}, you won another one!`)
+    olympicMedals++
 }
