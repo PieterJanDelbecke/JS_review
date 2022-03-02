e689dd38e6ea63b3c62b4384a322ce4b61f9bc4b
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-02 15:15:14 +1100

    codewars: areaOrPerimeter

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-function getMiddle(s)
-{
-    let half = Math.floor(s.length/2)
-    let arr = s.split("")
-    if (s.length % 2 === 0){
-        return arr[half-1].concat(arr[half])
-    } else {
-        return arr[half]
-    }
+function areaOrPerimeter(x, y) {
+  return x === y ? x * y : (x + y) * 2;
 }
 
-console.log(getMiddle("test"))
-console.log(getMiddle("testing"))
-console.log(getMiddle("middle"))
-console.log(getMiddle("A"))
+console.log(areaOrPerimeter(4, 4));
+console.log(areaOrPerimeter(6, 10));
