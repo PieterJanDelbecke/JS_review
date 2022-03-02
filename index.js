b906b4cf377b7fea8a9aef857f6e5fa1595a6452
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-02 16:43:59 +1100

    codewars: grow

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function sumMix(x){
-sum = 0 
-x.forEach(item => {
-    num = parseInt(item)
-    sum += num
-})
-return sum
+function grow(x){
+    total = 1
+    x.forEach(num => {
+        total *= num
+    })
+    return total
 }
 
-console.log(sumMix([9,3,'7',3]))
-console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(grow([1,2,3]))
+console.log(grow([4,1,1,1,4]))
+console.log(grow([2,2,2,2,2,2]))
