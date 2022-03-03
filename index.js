d356b5d3fe7b3948e8d312bfc91d02adf1b0c8ed
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-04 09:25:59 +1100

    review of Object constructors using prototype

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Object constructors
 
-function Person(name,age){
+function Person(name, age){
     this.name = name
     this.age = age
-    this.awesome = true
+    // this.shout = function(){
+    //     return `My name is ${this.name} and I'm ${age} years`
+    // }
 }
+Person.prototype.shout = function(){
+    return`my name is ${this.name} and I'm ${this.age}`
+}
+Person.prototype.role = "student"
+let person1 = new Person("Pete", 40)
 
-let instructor = new Person ("Janel", 25)
-
-console.log(instructor.age)
+console.log(person1)
+console.log(person1.shout())
+console.log(person1.role)
