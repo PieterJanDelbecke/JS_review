3ec6822d2bd5c206dbd0c23a26c5552bc08a1a5e
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-03 11:45:00 +1100

    codewars: squareDigits

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function disemvowel(str) {
-    return str.replace(/[aeiou]/gi, '');
-  }
+function squareDigits(num){
+    let newNum = num.toString().split('')
+    let arr = newNum.map(item =>{
+        return Math.pow(parseInt(item),2)
+    })
+    return parseInt(arr.join(''))
+}
 
-  console.log(disemvowel("Hello, World"))
+console.log(squareDigits(3212))
+console.log(squareDigits(2112))
+console.log(squareDigits(0))
