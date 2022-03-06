b9125fed08b834b35a45bc561c1f2943b4922cea
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-07 10:34:52 +1100

    codewars: sum of minimums

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function sequence(x){
-    arr = []
-    for (let i = 0; i < x; i++){
-        arr= [...arr, i+1]
-    }
-    return arr.sort()
+function sumOfMinimums(arr){
+    let sum = 0
+    arr.forEach(e => {
+        let min = Math.min(...e)
+        sum += min
+    })
+    return sum
 }
-console.log(sequence(16))
+
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
