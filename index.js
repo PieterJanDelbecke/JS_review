796c605430a2edda3d8f8686877fccad0817eb38
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-07 11:04:00 +1100

    codewars: Exes and Ohs

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function sumOfMinimums(arr){
-    let sum = 0
-    arr.forEach(e => {
-        let min = Math.min(...e)
-        sum += min
+function XO(str){
+    let arr = str.split("")
+    let x = 0, y = 0, num =0
+    arr = arr.map((e) => {
+       e = e.toLowerCase()
+       if (e === 'x'){
+        num++
+        x++
+       } else if (e === 'o'){
+           num++
+           y++
+       }
     })
-    return sum
+    if (num === 0){
+        return true
+    } else if (x === y){
+        return true
+    } else {
+        return false
+    }
 }
 
-console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+console.log(XO('xo'))
+console.log(XO('xxOo'))
+console.log(XO("xxxm"))
+console.log(XO("zpzpzpp"))
