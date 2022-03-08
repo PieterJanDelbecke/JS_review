dab5e5e1deac1a97c2931d18c4d30cefbff60f6d
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-08 17:35:46 +1100

    review of async/ await

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-function XO(str){
- str = str.toLowerCase().split('')
- return str.filter(e => e === 'x').length === str.filter( e => e === 'o').length
-}
+// Code wars
 
-console.log(XO('xo'))
-console.log(XO('xxOo'))
-console.log(XO("xxxm"))
-console.log(XO("zpzpzpp"))
