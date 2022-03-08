95fdd9ce41ddaad02e20427febf7288c6634726f
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-09 07:52:34 +1100

    review of promises

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-// Code wars
+// Promesis
 
+// setTimeout(() => { console.log("One")}, 1000)
+// console.log("two")
+
+// .then
+// .catch
+// .finally
+
+//has immutable statud => pending / reolved / rejected
+
+// let myFirstPromise = Promise.resolve("My first promise, yay!")
+//     myFirstPromise.then((value) => {console.log( value + "!!!")})
+
+// let myFirstRejection = Promise.reject("Oh no, error!")
+//     myFirstRejection.catch(reason => console.error(reason))
+
+function squareNumber(number) {
+    return new Promise((resolve,reject) => {
+        if (typeof number !== "number"){
+            reject(new Error("Input must be a number"))
+        }
+        resolve (number * number)
+    })
+}
+
+// squareNumber("a")
+// .then(squaredNumber => console.log("The squared number is" + squaredNumber))
+// .catch(error => console.log(error.message))
+// .finally(()=> console.log("The promise has finished"))
+
+// OR
+
+let squaredNumber = squareNumber("a")
+squaredNumber
+    .then( number => console.log(number))
+    .catch(error => console.log("Error: " + error.message))
