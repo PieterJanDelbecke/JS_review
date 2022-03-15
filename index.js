803795947975d6917baca73c210086e1be7e5632
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-16 08:38:30 +1100

    review of object methods

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const object1 ={ a:1, b:2, c:3}
+// Object.keys()
 
-// for(const property in object){
-//     console.log(`${property} : ${object[property]}`)
-// }
+const object1 = { a: 'something', b: 42, c: false }
 
-console.log(Object.values(object1))
+const keys =  Object.keys(object1)
+const values = Object.values(object1)
+
+console.log(keys)
+console.log(values)
+
+for (const property in object1){
+    console.log(`${property} is ${object1[property]}`)
+}
+
