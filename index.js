329734d6371191a7a3fb274528e684a887557150
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-21 18:07:55 +1100

    codeWars: getSum

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const object1 = '{"name":"Pete", "age":30, "awesome":true}'
-const object2 = {
-    firstName: "Pieter-Jan",
-    lastName: "Delbecke",
-    city: "Sydney",
-    age: 30,
-    carr: null
+function getSum(a, b) {
+  let total = 0;
+    if (a < b){
+        for (let i = a ; i <= b; i++) {
+          total += i;
+        }
+    } else {
+        for (let i)
+    }
+
+  return total
 }
 
-const newObject1 = JSON.parse(object1)
-const newObject2 = JSON.stringify(object2)
-
-console.log(newObject1)
-console.log(newObject2)
-
-// const json = '{"result":true, "count":42,"name":"pete"}';
-// const obj = JSON.parse(json);
-
-// console.log(obj)
-// console.log(obj.count);
-// // expected output: 42
-
-// console.log(obj.result);
-// // expected output: true
+console.log(getSum(0, 1))
+console.log("---------")
+console.log(getSum(1, 2))
+console.log("---------")
+console.log(getSum(-1, 2))
