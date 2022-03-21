fa57e320566fbd504fa998645bd7e01e2210c3e6
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-21 16:14:48 +1100

    review of JSON.parse() and JSON.stringify()

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// Object.entries()
-
-const object1 = { a: 10, b: 42, c: 50 }
-
-const arr = []
-
-for (const [key, value] of Object.entries(object1)){
-    if (value > 20 ){
-        arr.push ({
-            newKey: value
-        })
-    }
+const object1 = '{"name":"Pete", "age":30, "awesome":true}'
+const object2 = {
+    firstName: "Pieter-Jan",
+    lastName: "Delbecke",
+    city: "Sydney",
+    age: 30,
+    carr: null
 }
 
-console.log(arr)
+const newObject1 = JSON.parse(object1)
+const newObject2 = JSON.stringify(object2)
+
+console.log(newObject1)
+console.log(newObject2)
+
+// const json = '{"result":true, "count":42,"name":"pete"}';
+// const obj = JSON.parse(json);
+
+// console.log(obj)
+// console.log(obj.count);
+// // expected output: 42
+
+// console.log(obj.result);
+// // expected output: true
